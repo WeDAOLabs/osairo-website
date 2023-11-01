9024a6483bb046913b196a86925f33310192fc6b
enixlee <405284695@qq.com>
2023-11-01 17:19:11 +0800

    modify

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "app.happiairdrop.xyz",
+      },
+      {
+        protocol: "https",
+        hostname: "img.shields.io",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
