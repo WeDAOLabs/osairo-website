17595608eb330a93e40eed714aaad31e76ab5f4e
enixlee <405284695@qq.com>
2023-11-06 19:12:02 +0800

    add swiper

--- src/components/home/page.tsx
+++ src/components/home/page.tsx
@@ -1,3 +1,10 @@
+"use client";
+import React from "react";
+import "swiper/css";
+import "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Mousewheel } from "swiper/modules";
+
 import HomePage0 from "./home.page.0";
 import HomePage1 from "./home.page.1";
 import HomePage2 from "./home.page.2";
@@ -8,19 +15,41 @@ import HomePage6 from "./home.page.6";
 
 export default function Home() {
   return (
-    <div
+    <Swiper
+      direction={"vertical"}
+      spaceBetween={10}
+      mousewheel={true}
+      modules={[Mousewheel]}
+      onSlideChange={() => console.log("slide change")}
+      onSwiper={(swiper) => console.log(swiper)}
+      pagination={false}
+      autoHeight={true}
+      speed={500}
       style={{
-        width: "100%",
-        height: "100%",
+        height: "100vh",
       }}
     >
-      <HomePage0 />
-      <HomePage1 />
-      <HomePage2 />
-      <HomePage3 />
-      <HomePage4 />
-      <HomePage5 />
-      <HomePage6 />
-    </div>
+      <SwiperSlide>
+        <HomePage0 />
+      </SwiperSlide>
+      <SwiperSlide>
+        <HomePage1 />
+      </SwiperSlide>
+      <SwiperSlide>
+        <HomePage2 />
+      </SwiperSlide>
+      <SwiperSlide>
+        <HomePage3 />
+      </SwiperSlide>
+      <SwiperSlide>
+        <HomePage4 />
+      </SwiperSlide>
+      <SwiperSlide>
+        <HomePage5 />
+      </SwiperSlide>
+      <SwiperSlide>
+        <HomePage6 />
+      </SwiperSlide>
+    </Swiper>
   );
 }
