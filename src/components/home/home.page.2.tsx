b67b72ab8c1ec8a1517a84668d0802c9b224677f
enixlee <405284695@qq.com>
2023-11-08 11:57:47 +0800

    add page 2

--- src/components/home/home.page.2.tsx
+++ src/components/home/home.page.2.tsx
@@ -24,8 +24,82 @@ export default function HomePage2() {
               className={panelStyle.panelCircleContainer}
               style={{
                 background: "#d1dcfe",
+                display: "flex",
               }}
-            ></div>
+            >
+              <div
+                className="page-left"
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flex: "56%",
+                  maxWidth: "56%",
+                }}
+              ></div>
+              <div
+                className="page-right"
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flex: "44%",
+                  maxWidth: "44%",
+                }}
+              >
+                <div
+                  style={{
+                    width: 460,
+                    height: 642,
+                    background: "#474b57",
+                    borderRadius: 30,
+                    position: "relative",
+                  }}
+                >
+                  <label
+                    style={{
+                      position: "absolute",
+                      width: 383,
+                      height: 170,
+                      fontFamily: "Arial Black",
+                      fontStyle: "normal",
+                      fontWeight: 900,
+                      fontSize: 60,
+                      color: "white",
+                      left: 45,
+                      top: 25,
+                    }}
+                  >
+                    Economic foundation
+                  </label>
+                  <text
+                    style={{
+                      position: "absolute",
+                      width: 384,
+                      height: 224,
+
+                      fontFamily: "Arial Hebrew",
+                      fontStyle: "normal",
+                      fontWeight: 700,
+                      fontSize: 30,
+
+                      color: "white",
+
+                      left: 45,
+                      top: 361,
+                    }}
+                  >
+                    Osario Island Resource Token
+                    <br />
+                    <br />
+                    $OIT
+                    <br />
+                    <br />
+                    Total supply: 100 billion pieces
+                  </text>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </PageFitter>
