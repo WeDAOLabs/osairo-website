959073762fc9499fd1f3f2b66379e8c875f28aaa
enixlee <405284695@qq.com>
2023-11-08 19:41:54 +0800

    add page 6

--- src/components/home/home.page.6.tsx
+++ src/components/home/home.page.6.tsx
@@ -1,6 +1,7 @@
-import mainStyle from "@/styles/main.module.css";
+// import mainStyle from "@/styles/main.module.css";
 import panelStyle from "./page.module.css";
 import PageFitter from "../page-fitter/page-fitter";
+import Image from "next/image";
 
 export default function HomePage6() {
   return (
@@ -36,8 +37,49 @@ export default function HomePage6() {
             top: 560,
             left: 0,
             width: "100vw",
+            alignItems: "center",
+            justifyContent: "center",
           }}
-        ></div>
+        >
+          <div
+            style={{
+              width: 1420,
+              height: 560,
+              position: "relative",
+            }}
+          >
+            <text
+              style={{
+                position: "absolute",
+                width: 550,
+                height: 170,
+
+                fontFamily: "Arial Black",
+                fontStyle: "normal",
+                fontWeight: 900,
+                fontSize: 60,
+
+                color: "#FFEBBB",
+
+                left: 97,
+                top: 46,
+              }}
+            >
+              Osairo Adventure game
+            </text>
+            <Image
+              src="/logo-big.png"
+              width={226.5}
+              height={111}
+              alt="osairo"
+              style={{
+                position: "absolute",
+                top: 329,
+                left: 1178,
+              }}
+            />
+          </div>
+        </div>
       </PageFitter>
     </>
   );
