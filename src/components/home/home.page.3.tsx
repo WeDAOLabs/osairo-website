3938a465f2a3c7651c4b36e9206ef2ab20198fe0
enixlee <405284695@qq.com>
2023-11-08 12:12:46 +0800

    add page 4

--- src/components/home/home.page.3.tsx
+++ src/components/home/home.page.3.tsx
@@ -24,8 +24,78 @@ export default function HomePage3() {
               className={panelStyle.panelCircleContainer}
               style={{
                 background: "#dbcdf2",
+                display: "flex",
               }}
-            ></div>
+            >
+              <div
+                className="left"
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flex: "44%",
+                  maxWidth: "44%",
+                }}
+              >
+                <div
+                  style={{
+                    width: 460,
+                    height: 642,
+                    background: "#474b57",
+                    borderRadius: 30,
+                    position: "relative",
+                  }}
+                >
+                  <label
+                    style={{
+                      position: "absolute",
+                      width: 415,
+                      height: 77,
+                      fontFamily: "Arial Black",
+                      fontStyle: "normal",
+                      fontWeight: 900,
+                      fontSize: 60,
+                      color: "white",
+                      left: 45,
+                      top: 25,
+                    }}
+                  >
+                    27 precious resources
+                  </label>
+                  <text
+                    style={{
+                      position: "absolute",
+                      width: 383,
+                      height: 192,
+
+                      fontFamily: "Arial Hebrew",
+                      fontStyle: "normal",
+                      fontWeight: 700,
+                      fontSize: 30,
+
+                      color: "white",
+
+                      left: 39,
+                      top: 355,
+                    }}
+                  >
+                    Resources are the core output of mining and the economic
+                    driver, following the ERC1155 standard to ensure
+                    interactivity.
+                  </text>
+                </div>
+              </div>
+              <div
+                className="right"
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flex: "56%",
+                  maxWidth: "56%",
+                }}
+              ></div>
+            </div>
           </div>
         </div>
       </PageFitter>
